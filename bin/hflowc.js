3f4638cda2d529e11ca7f9df50009ac89b6f2621
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-05-24 20:12:02 +0200

    Basic structure for hflowc script.

--- bin/hflowc.js
+++ bin/hflowc.js
@@ -1,19 +1,40 @@
 #!/usr/bin/env node
 
-var docopt = require('docopt').docopt;
+var docopt = require('docopt').docopt,
+    atmoClientFactory = require('../lib/atmosphere_client'),
+    fs = require('fs');
 
-var doc='\
+var doc = '\
 Usage:\n\
     hflowc run \<wf_file.json\>\n\
 ';
 
+function readProxy(proxyLocation, cb) {
+    fs.readFile(proxyLocation, function (err, proxyContents) {
+        if (err) {
+            cb(err);
+            return;
+        }
+        cb(null, proxyContents);
+    });
+}
+
 var opts = docopt(doc);
 
-var proxyLocation = process.env.X509_USER_PROXY;
-var proxyLocation = process.env.ATMOSPHERE_URL;
+var proxyLocation = process.env.X509_USER_PROXY ? process.env.X509_USER_PROXY : 'proxy.pem';
+var atmoLocation = process.env.ATMOSPHERE_URL ? process.env.ATMOSPHERE_URL : 'https://cloud-dev.plgrid.pl/api/v1';
 
-var atmoClientFactory = require('../lib/atmosphere_client');
+if (opts.run) {
+    readProxy(proxyLocation, function (err, proxy) {
+        if (err) {
+            console.log('Error reading proxy! forgot to do a voms-proxy-init?');
+            return;
+        }
 
-atmoClient = atmoClientFactory.createClient('http://localhost', 'ASCASVCAS=');
+        var atmoClient = atmoClientFactory.createClient(atmoLocation, proxy);
 
-atmoClient.createVm(98, {});
+        atmoClient.createVm(98, {});
+        //TODO: orchestrate rest of wf
+    });
+}
+
