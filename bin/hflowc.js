ba06285ec36a1de7a3ecd69e7182877dd58d6621
Maciej Pawlik <m.pawlik@cyfronet.pl>
2015-05-25 12:09:36 +0200

    Add skel for newApplianceSet operation.

--- bin/hflowc.js
+++ bin/hflowc.js
@@ -9,6 +9,9 @@ Usage:\n\
     hflowc run \<wf_file.json\>\n\
 ';
 
+var wfMainId = 19;
+var wfWorkerId = 19;
+
 function readProxy(proxyLocation, cb) {
     fs.readFile(proxyLocation, function (err, proxyContents) {
         if (err) {
@@ -33,8 +36,19 @@ if (opts.run) {
 
         var atmoClient = atmoClientFactory.createClient(atmoLocation, proxy);
 
-        atmoClient.createVm(98, {});
-        //TODO: orchestrate rest of wf
+        atmoClient.newApplianceSet(
+            [{
+                applianceId: wfMainId,
+                params: null,
+                vms: [{cpu: 1, mem: 512}]
+            }], function (err) {
+                if (err) {
+                    console.log('Error creating appliance!', err);
+                    return;
+                }
+                console.log('Appliance created successfully!')
+            }
+        );
     });
 }
 
